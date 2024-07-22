3a13d61d509cfe56faa7ba6fa1e778d8a6c69a12
Richardmelo20 <118909224+Richardmelo20@users.noreply.github.com>
2024-07-22 19:57:03 -0300

    :sparkles: feat/header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,33 @@
-<template lang="">
-  <div></div>
+<template>
+  <navBar :logo="logo" :alt="alt" />
+  <router-view />
+  <appFooter />
 </template>
 <script>
+import navBar from './components/navBar.vue'
+import appFooter from './components/appFooter.vue'
+
 export default {
   name: 'App',
+  components: {
+    navBar,
+    appFooter
+  },
   data() {
-    return {}
+    return {
+      logo: '/images/logo.png',
+      alt: 'Make your burger'
+    }
   }
 }
 </script>
-<style lang="scss" scope></style>
+<style lang="scss">
+.main-container {
+  margin: 50px;
+  min-height: 250px;
+
+  &__title {
+    @include title($align: center, $margin: 0 0 30px 0);
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import home from '../views/pageDefault.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home'
+      name: 'home',
+      component: home
     },
     {
-      path: '/about',
-      name: 'about'
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+      path: '/ordersList',
+      name: 'pedidos',
+      component: () => import(/*webpackChunkName: "pedidos" */ '../views/ordersList.vue')
     }
   ]
 })
